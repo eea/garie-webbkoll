f4a3e088be412da984f85a3253bb4897210352b8
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-02-28 17:07:05 +0200

    Refs #100860; Generate webbkoll reports in json format

--- src/index.js
+++ src/index.js
@@ -1,23 +1,62 @@
 const garie_plugin = require('garie-plugin')
+const fs = require('fs-extra');
 const path = require('path');
 const config = require('../config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const serveIndex = require('serve-index');
+const request = require('request-promise');
+
+const webbkoll_backend_uri = "http://"+process.env.BACKEND_HOST + ":" + process.env.BACKEND_PORT;
+
+const getDataFromBackend = async( url ) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await request({
+                method: 'GET',
+                uri: webbkoll_backend_uri,
+                qs: {
+                  'fetch_url': url
+                },
+                json: true,
+                resolveWithFullResponse: true
+            });
+            resolve(response.body);
+        } catch (err) {
+            console.log(`Failed to get data for ${url}`, err);
+            reject(`Failed to get data for ${url}`);
+        }
+    });
+}
+
 
 const myGetData = async (item) => {
     const { url } = item.url_settings;
     return new Promise(async (resolve, reject) => {
         try {
-            console.log("here");
-            data = {}
+            const { reportDir } = item;
+            const reportFolder = garie_plugin.utils.helpers.reportDirNow(reportDir);
+
+            const json_data = await getDataFromBackend(url);
+
+            var json_file = path.join(reportFolder, 'webbkoll.json');
+
+            fs.outputJson(json_file, json_data, {spaces: 2})
+            .then(() => console.log(`Saved webbkoll json file for ${url}`))
+            .catch(err => {
+              console.log(err)
+            })
+
+
+//            await writeReport(jsonReport, reportDirNow)
+            var data = {}
             resolve(data);
         } catch (err) {
             console.log(`Failed to get data for ${url}`, err);
             reject(`Failed to get data for ${url}`);
         }
     });
-};
+}
 
 
 
