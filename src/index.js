756271d6381f29c86f3634aa4a755f207b08ec31
Alex Morega <alex@grep.ro>
2020-03-23 16:38:19 +0200

    Scan on demand

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ const garie_plugin = require('garie-plugin')
 const fs = require('fs-extra');
 const path = require('path');
 const config = require('../config');
-const express = require('express');
-const bodyParser = require('body-parser');
-const serveIndex = require('serve-index');
 const request = require('request-promise');
 const sleep = require('sleep-promise');
 const scrape = require('website-scraper');
@@ -337,37 +334,27 @@ const getData = async (item) => {
 console.log("Start");
 
 
-const app = express();
-app.use('/reports', express.static('reports'), serveIndex('reports', { icons: true }));
-
 const main = async () => {
-  return new Promise(async (resolve, reject) => {
-    try{
-      await garie_plugin.init({
-        getData:getData,
-        getMeasurement:myEmptyGetMeasurement,
-        db_name:'webbkoll',
-        plugin_name:'webbkoll',
-        report_folder_name:'webbkoll-results',
-        app_root: path.join(__dirname, '..'),
-        config:config
-      });
-    }
-    catch(err){
-      reject(err);
-    }
-  });
+  try{
+    const { app } = await garie_plugin.init({
+      getData:getData,
+      getMeasurement:myEmptyGetMeasurement,
+      db_name:'webbkoll',
+      plugin_name:'webbkoll',
+      report_folder_name:'webbkoll-results',
+      app_root: path.join(__dirname, '..'),
+      config:config,
+      onDemand: true
+    });
+    app.listen(3000, () => {
+      console.log('Application listening on port 3000');
+    });
+  }
+  catch(err){
+    console.log(err);
+  }
 }
 
 if (process.env.ENV !== 'test') {
-  const server = app.listen(3000, async () => {
-    console.log('Application listening on port 3000');
-    try{
-      await main();
-    }
-    catch(err){
-      console.log(err);
-      server.close();
-    }
-  });
+  main();
 }
